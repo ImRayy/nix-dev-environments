e4d2a36753a4fbcad1c5346344b569badcde80fc
RAY <souradeep_roy@pm.me>
2024-04-21 23:38:15 +0530

    Added android-sdk

---
+++ android-sdk/sdk.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+with pkgs;
+
+let
+  android-nixpkgs = callPackage <android-nixpkgs> { };
+
+  android-sdk = android-nixpkgs.sdk (sdkPkgs:
+    with sdkPkgs; [
+      cmdline-tools-latest
+      build-tools-34-0-0
+      platform-tools
+      platforms-android-34
+      emulator
+    ]);
+
+in mkShell rec {
+  ANDORID_SDK_ROOT = "${android-sdk}/libexec/android-sdk";
+  buildInputs = [ android-sdk ];
+}
